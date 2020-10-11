b9136667b14678393f3dcfa1912106fbe1803a9e
Tiago Vaccari <tiago.vaccari@outlook.com>
2020-10-11 20:49:01 +0100

    Logo Included

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
+import {Image} from 'react-native';
 
 import {Container} from './styles';
+
+import logoImg from '../../assets/logo.png';
 
 const SignIn: React.FC = () => {
 
     return (
-        <Container />
+        <Container>
+            <Image source={logoImg} />
+        </Container>
     );
-
-}
+};
 
 export default SignIn;
