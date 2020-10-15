aef12cd69d4e160de2ad5e4df47533a29abd1b3d
Tiago Vaccari <tiago.vaccari@outlook.com>
2020-10-15 20:37:59 +0100

    Create account page - created

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,13 +1,50 @@
 import React from 'react';
+import {Image, View, ScrollView, KeyboardAvoidingView, Platform} from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
 
-import {Container} from './styles';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import logoImg from '../../assets/logo.png';
+
+import {Container, 
+        Title, 
+        ForgotPassword, 
+        ForgotPasswordText, 
+        BackToSignIn,
+        BackToSignInText} from './styles';
 
 const SignUp: React.FC = () => {
 
     return (
-        <Container />
-    );
+        <>
+            <KeyboardAvoidingView 
+                style={{flex: 1}}
+                behavior={Platform.OS == 'ios' ? 'padding' : undefined}
+                enabled>
 
-}
+                <ScrollView
+                    keyboardShouldPersistTaps="handled"
+                    contentContainerStyle={{ flex: 1 }}>
+                    <Container>
+                        <Image source={logoImg} />
+                        <View>
+                            <Title>Create Account</Title>
+                        </View>
+
+                        <Input name="name" icon="user" placeholder="User Name" />
+                        <Input name="email" icon="mail" placeholder="E-mail" />
+                        <Input name="password" icon="lock" placeholder="Password" />
+                        <Button onPress={() => {console.log("Entered")}}>Enter</Button>
+                    </Container>
+                </ScrollView>
+            </KeyboardAvoidingView>
+            <BackToSignIn onPress={() => {}}>
+                <Icon name="arrow-left" size={20} color="#ff9000"/>
+                <BackToSignInText>Back</BackToSignInText>
+            </BackToSignIn>
+        </>
+    );
+};
 
 export default SignUp;
