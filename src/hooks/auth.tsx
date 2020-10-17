878e3647b86eb330640b662872f9c811fcef25ec
Tiago Vaccari <tiago.vaccari@outlook.com>
2020-10-17 21:18:44 +0100

    Dashboard view created

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -15,6 +15,7 @@ interface AuthState {
 
 interface AuthContextData {
     user: object;
+    loading: boolean;
     signIn(credentials: SignInCredentials): Promise<void>;
     signOut(): void;
 }
@@ -24,6 +25,7 @@ const AuthContext = createContext<AuthContextData>({} as AuthContextData);
 const AuthProvider: React.FC = ({children}) => {
 
     const [data, setData] = useState<AuthState>({} as AuthState);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
 
@@ -33,6 +35,8 @@ const AuthProvider: React.FC = ({children}) => {
             if (token && user) {
                 setData({token, user: JSON.parse(user)});
             }
+
+            setLoading(false);
         }
 
         loadStorageData();
@@ -60,7 +64,7 @@ const AuthProvider: React.FC = ({children}) => {
 
     return (
 
-        <AuthContext.Provider value={{ user: data.user, signIn, signOut }}>
+        <AuthContext.Provider value={{ user: data.user, loading, signIn, signOut }}>
             {children}
         </AuthContext.Provider>
 
