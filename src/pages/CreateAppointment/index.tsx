6d7c1f2bce7f7e1171bf0d4ec6467d844951d201
Tiago Vaccari <tiago.vaccari@outlook.com>
2020-11-25 15:55:55 +0000

    Barber list created

--- src/pages/CreateAppointment/index.tsx
+++ src/pages/CreateAppointment/index.tsx
@@ -1,10 +1,34 @@
-import React from 'react';
-import {View} from 'react-native';
+import { useRoute } from '@react-navigation/native';
+import React, { useCallback } from 'react';
+import Icon from 'react-native-vector-icons/Feather';
+import { useAuth } from '../../hooks/auth';
+import { useNavigation } from '@react-navigation/native';
+import {Container, Header, BackButton, HeaderTitle, UserAvatar} from './styles';
+
+interface RouteParams{
+    providerId: string;
+}
 
 const CreateAppointment: React.FC = () => {
+    const {user} = useAuth();
+    const route = useRoute();
+    const {providerId} = route.params as RouteParams;
+    const {goBack} = useNavigation();
+
+    const navigateBack = useCallback(() => {
+        goBack();
+    }, [goBack]);
 
     return (
-        <View />
+        <Container>
+            <Header>
+                <BackButton onPress={navigateBack}>
+                    <Icon name="chevron-left" size={24} color="#999591"></Icon>
+                </BackButton>
+                <HeaderTitle>Barbers</HeaderTitle>
+                <UserAvatar source={{uri: user.avatar_url}}></UserAvatar>
+            </Header>
+        </Container>
     );
 }
 
