882f6c3294f4c3104316706b6a570a526c0b3922
Tiago Vaccari <tiago.vaccari@outlook.com>
2020-11-26 10:52:06 +0000

    Appointment Creatio added

--- src/pages/CreateAppointment/index.tsx
+++ src/pages/CreateAppointment/index.tsx
@@ -1,12 +1,37 @@
 import { useRoute } from '@react-navigation/native';
-import React, { useCallback } from 'react';
+import React, { useCallback, useState } from 'react';
 import Icon from 'react-native-vector-icons/Feather';
+import DateTimePicker from '@react-native-community/datetimepicker';
 import { useAuth } from '../../hooks/auth';
 import { useNavigation } from '@react-navigation/native';
-import {Container, Header, BackButton, HeaderTitle, UserAvatar} from './styles';
+import {
+        Container, 
+        Header, 
+        BackButton, 
+        HeaderTitle, 
+        UserAvatar,
+        ProvidersListContainer,
+        ProvidersList,
+        ProviderContainer,
+        ProviderAvatar,
+        ProviderName,        
+        Calendar,
+        OpenDatePickerButton,
+        OpenDatePickerButtonText,
+        CalendarTitle
+    } from './styles';
+import { useEffect } from 'react';
+import api from '../../services/api';
+import { Platform } from 'react-native';
 
 interface RouteParams{
     providerId: string;
+}
+
+export interface Provider {
+    id: string;
+    name: string;
+    avatar_url: string;
 }
 
 const CreateAppointment: React.FC = () => {
@@ -15,9 +40,45 @@ const CreateAppointment: React.FC = () => {
     const {providerId} = route.params as RouteParams;
     const {goBack} = useNavigation();
 
+    const [providers, setProviders] = useState<Provider[]>([]);
+
+    const [selectedProvider, setSelectedProvider] = useState(providerId);
+
+    const [showDatePicker, setShowDatePicker] = useState(false);
+
+    const [selectedDate, setSelectedDate] = useState(new Date());
+
+    useEffect(() => {
+        api.get('/providers')
+            .then(response => {
+                setProviders(response.data);
+            })
+    }, [])
+
     const navigateBack = useCallback(() => {
         goBack();
     }, [goBack]);
+
+    const handleSelecteProvider = useCallback((selectedProviderId: string) => {
+        setSelectedProvider(selectedProviderId);
+
+    },[]);
+
+    const handleToggleDatePicker = useCallback(() => {
+        setShowDatePicker((state) => !state);
+    },[],);
+
+    const handleDateChanged = useCallback((event: any, date: Date | undefined) => {
+
+        if(Platform.OS === 'android'){
+            setShowDatePicker(false);
+        }
+
+        if (date) {
+            setSelectedDate(date);    
+        }
+
+    }, []);
 
     return (
         <Container>
@@ -28,6 +89,38 @@ const CreateAppointment: React.FC = () => {
                 <HeaderTitle>Barbers</HeaderTitle>
                 <UserAvatar source={{uri: user.avatar_url}}></UserAvatar>
             </Header>
+            <ProvidersListContainer>
+                <ProvidersList
+                    horizontal
+                    showsHorizontalScrollIndicator={false}
+                    data={providers}
+                    keyExtractor={(provider) => provider.id}
+                    renderItem={({item: provider}) => (
+                        <ProviderContainer
+                            onPress={() => handleSelecteProvider(provider.id)}
+                            selected={provider.id === selectedProvider}
+                        >
+                            <ProviderAvatar source={{uri: provider.avatar_url}}></ProviderAvatar>
+                            <ProviderName selected={provider.id === selectedProvider}>{provider.name}</ProviderName>
+                        </ProviderContainer>
+                    )}>
+                </ProvidersList>
+            </ProvidersListContainer>
+
+            <Calendar>
+                <CalendarTitle>Date</CalendarTitle>
+                <OpenDatePickerButton onPress={handleToggleDatePicker}>
+                    <OpenDatePickerButtonText>Select another date</OpenDatePickerButtonText>
+                </OpenDatePickerButton>
+                {showDatePicker && (<DateTimePicker 
+                    mode="date"
+                    display="calendar"
+                    onChange={handleDateChanged}
+                    textColor="#f4ede8"
+                    value={selectedDate}
+                >
+                </DateTimePicker>)}
+            </Calendar>
         </Container>
     );
 }
--- src/pages/CreateAppointment/styles.ts
+++ src/pages/CreateAppointment/styles.ts
@@ -1,6 +1,16 @@
-import { Platform } from 'react-native';
+import { Platform, FlatList } from 'react-native';
 import styled from 'styled-components/native';
+import { Provider } from './index';
 import {getStatusBarHeight} from 'react-native-iphone-x-helper';
+import { RectButton } from 'react-native-gesture-handler';
+
+interface ProviderContainerProps{
+    selected: boolean;
+}
+
+interface ProviderNameProps{
+    selected: boolean;
+}
 
 export const Container = styled.View`
     flex: 1;
@@ -34,3 +44,59 @@ export const UserAvatar = styled.Image`
     margin-left: auto;
 `;
 
+export const ProvidersListContainer = styled.View`
+    height: 112px;
+`;
+
+export const ProvidersList = styled(FlatList as new () => FlatList<Provider>)`
+    padding: 32px 24px;
+`;
+
+export const ProviderContainer = styled(RectButton)<ProviderContainerProps>`
+    background: ${props => props.selected ? '#ff9000' : '#3e3b47'};
+    flex-direction: row;
+    align-items: center;
+    padding: 8px 12px;
+    margin-right: 16px;
+    border-radius: 10px;
+`;
+
+export const ProviderAvatar = styled.Image`
+    width: 32px;
+    height: 32px;
+    border-radius: 16px;
+`;
+
+export const ProviderName = styled.Text<ProviderNameProps>`
+    margin-left: 8px;
+    font-family: 'RobotoSlab-Medium';
+    font-size: 16px;
+    color: ${props => props.selected ? '#232129' : '#f4ede8'};
+`;
+
+export const Calendar = styled.View`
+
+`;
+
+export const OpenDatePickerButton = styled(RectButton)`
+    height: 46px;
+    background: #ff9000;
+    border-radius: 10px;
+    align-items: center;
+    justify-content: center;
+    margin: 0 24px;
+`;
+
+export const OpenDatePickerButtonText = styled.Text`
+    font-family: 'RobotoSlab-Medium';
+    font-size: 16px;
+    color: #232129;
+`;
+
+
+export const CalendarTitle = styled.Text`
+    font-family: 'RobotoSlab-Medium';
+    color: #f4ede8;
+    font-size: 24px;
+    margin: 0 24px 24px;
+`;
